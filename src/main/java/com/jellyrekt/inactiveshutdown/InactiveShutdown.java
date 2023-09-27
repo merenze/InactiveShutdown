b1db868d8972cfb2fff04a002e04aa4fdfa51b4d
Matthew Renze <merenze@gmail.com>
2023-09-27 18:07:03 -0500

    Add dummy commands

--- src/main/java/com/jellyrekt/inactiveshutdown/InactiveShutdown.java
+++ src/main/java/com/jellyrekt/inactiveshutdown/InactiveShutdown.java
@@ -1,10 +1,52 @@
 package com.jellyrekt.inactiveshutdown;
 
+import com.jellyrekt.commandtree.CommandTree;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class InactiveShutdown extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Hello, World!");
+        getLogger().info(registerCommands().toString());
+    }
+
+    private CommandTree registerCommands() {
+        CommandTree commandTree = new CommandTree(this)
+            .setPermissionDeniedMessage("Unknown command. Type \"/help\" for help.");
+
+        commandTree.add("inactiveshutdown")
+            .addAliases("insh")
+            .setPermission("inactiveshutdown.command.base");
+
+        commandTree.add("inactiveshutdown reloadconfig")
+            .setPermission("inactiveshutdown.command.reloadconfig");
+
+        commandTree.add("inactiveshutdown afterplayerleave")
+            .setPermission("inactiveshutdown.command.afterplayerleave");
+
+        commandTree.add("inactiveshutdown afterplayerleave enable")
+            .setPermission("inactiveshutdown.command.afterplayerleave.enable");
+
+        commandTree.add("inactiveshutdown afterplayerleave disable")
+            .setPermission("inactiveshutdown.command.afterplayerleave.disable");
+
+        commandTree.add("inactiveshutdown afterplayerleave delay")
+            .setPermission("inactiveshutdown afterplayerleave delay");
+
+        commandTree.add("inactiveshutdown afterstartup")
+            .setPermission("inactiveshutdown.command.afterstartup");
+
+        commandTree.add("inactiveshutdown afterstartup enable")
+            .setPermission("inactiveshutdown.command.afterstartup.enable");
+
+        commandTree.add("inactiveshutdown afterstartup disable")
+            .setPermission("inactiveshutdown.command.afterstartup.disable");
+
+        commandTree.add("inactiveshutdown afterstartup delay")
+            .setPermission("inactiveshutdown.command.afterstartup.delay");
+
+        commandTree.register();
+
+        return commandTree;
     }
 }
